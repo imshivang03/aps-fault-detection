4ffd8ba910eb481c0a3b14dec42f7c5e4ce0d639
imshivang03 <shivang.cse.nitnagaland@gmail.com>
2023-04-12 10:11:31 +0530

    Updated artifact file

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 @dataclass
-class DataIngestionArtiact:
+class DataIngestionArtifact:
     feature_store_file_path: str
     train_file_path: str
     test_file_path: str
